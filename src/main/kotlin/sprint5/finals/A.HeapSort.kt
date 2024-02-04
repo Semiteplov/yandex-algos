53325fc857dcc6c874e98561294915280dbae7dc
semiteplov <semiteplov@gmail.com>
2024-02-04 18:19:37 +0300

    Sprint 5. Finals - refactoring

--- src/main/kotlin/sprint5/finals/A.HeapSort.kt
+++ src/main/kotlin/sprint5/finals/A.HeapSort.kt
@@ -4,21 +4,16 @@ import sprint3.finals.swap
 import java.io.BufferedWriter
 
 /*
-    https://contest.yandex.ru/contest/24810/run-report/106552979/
+    https://contest.yandex.ru/contest/24810/run-report/106558187/
 
     Эта задача реализует алгоритм сортировки кучей (Heapsort) для таблицы результатов соревнования по программированию.
     Сортировка выполняется на основе количества решенных задач, размера штрафа и лексикографического порядка логинов участников.
     Каждый участник представлен классом Participant, содержащим его логин, количество решенных задач и штраф.
 
     -- ПРИНЦИП РАБОТЫ --
-    Для сортировки используется структура данных - максимальная куча. Компаратор для кучи устроен таким образом,
-    что участник с наибольшим количеством решенных задач находится на вершине кучи.
-    При равенстве количества решенных задач выше будет тот, у кого меньше штраф.
-    Если и штрафы одинаковы, сравниваются логины участников.
-    Это обеспечивает необходимый порядок сортировки в соответствии с условиями.
-
-    В процессе добавления нового участника в кучу используется операция просеивания вверх, чтобы восстановить свойства максимальной кучи.
-    При извлечении элемента из кучи (получении участника с максимальным приоритетом) используется просеивание вниз.
+    В алгоритме применяются операции просеивания вверх и вниз непосредственно к массиву участников.
+    Функция add добавляет новый элемент в массив и просеивает его вверх до корректной позиции для max-heap.
+    Функция popMax извлекает максимальный элемент (по компаратору) из массива, обеспечивая правильный порядок сортировки.
 
     -- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
     Алгоритм корректно сортирует участников в соответствии с заданными критериями благодаря правильно реализованным операциям просеивания вверх и вниз.
@@ -30,63 +25,56 @@ import java.io.BufferedWriter
     Извлечение всех элементов из кучи также занимает O(n log n) времени.
 
     -- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
-    Пространственная сложность алгоритма составляет O(n), так как все участники хранятся внутри кучи.
-    Дополнительное пространство используется для компаратора и временных переменных при операциях просеивания,
-    но это не влияет на общую пространственную сложность.
+    Поскольку все операции выполняются непосредственно на массиве участников, дополнительная пространственная сложность алгоритма составляет O(1).
+    Это делает алгоритм пространственно эффективным, так как не требует дополнительного места,
+    за исключением небольшого количества временных переменных для операций просеивания.
 
 */
 
 private data class Participant(val login: String, val solvedProblems: Int, val penalties: Int)
 
-class Heap<T>(private val comparator: Comparator<T>) {
+private fun <T> MutableList<T>.add(element: T, comparator: Comparator<T>) {
+    this.add(element)
+    this.siftUp(this.lastIndex, comparator)
+}
 
-    private val heap = mutableListOf<T>()
+private fun <T> MutableList<T>.siftUp(index: Int, comparator: Comparator<T>) {
+    if (index == 0) return
+    val parentIndex = (index - 1) / 2
 
-    fun add(item: T) {
-        heap.add(item)
-        siftUp(heap.lastIndex)
+    if (comparator.compare(this[index], this[parentIndex]) > 0) {
+        this.swap(index, parentIndex)
+        return siftUp(parentIndex, comparator)
     }
+}
 
-    private fun siftUp(index: Int) {
-        if (index == 0) return
+private fun <T> MutableList<T>.popMax(comparator: Comparator<T>): T {
+    val maxItem = this.first()
+    val lastIndex = this.lastIndex
+    this[0] = this[lastIndex]
+    this.removeAt(this.lastIndex)
 
-        val parentIndex = (index - 1) / 2
+    if (this.isNotEmpty()) siftDown(0, comparator)
 
-        if (comparator.compare(heap[index], heap[parentIndex]) > 0) {
-            heap.swap(index, parentIndex)
-            return siftUp(parentIndex)
-        }
-    }
+    return maxItem
+}
 
-    fun popMax(): T {
-        val maxItem = heap.first()
-        val lastIndex = heap.lastIndex
-        heap[0] = heap[lastIndex]
-        heap.removeAt(heap.lastIndex)
+private fun <T> MutableList<T>.siftDown(index: Int, comparator: Comparator<T>) {
+    val leftChildIndex = index * 2 + 1
+    val rightChildIndex = index * 2 + 2
+    val lastItemIndex = this.lastIndex
 
-        if (heap.isNotEmpty()) siftDown(0)
+    if (leftChildIndex > lastItemIndex) return
 
-        return maxItem
-    }
+    val largestItemIndex = if (rightChildIndex <= lastItemIndex && comparator.compare(
+            this[rightChildIndex],
+            this[leftChildIndex]
+        ) > 0
+    ) rightChildIndex else leftChildIndex
 
-    private fun siftDown(index: Int = 1) {
-        val leftChildIndex = index * 2 + 1
-        val rightChildIndex = index * 2 + 2
-        val lastItemIndex = heap.lastIndex
-
-        // no children
-        if (leftChildIndex > lastItemIndex) return
-
-        val largestItemIndex = if (rightChildIndex <= lastItemIndex && comparator.compare(
-                heap[rightChildIndex],
-                heap[leftChildIndex]
-            ) > 0
-        ) rightChildIndex else leftChildIndex
-
-        if (comparator.compare(heap[largestItemIndex], heap[index]) > 0) {
-            heap.swap(index, largestItemIndex)
-            return siftDown(largestItemIndex)
-        }
+    if (comparator.compare(this[largestItemIndex], this[index]) > 0) {
+        this.swap(index, largestItemIndex)
+        return siftDown(largestItemIndex, comparator)
     }
 }
 
@@ -108,16 +96,17 @@ fun main() {
     val participantsComparator = compareBy<Participant> { it.solvedProblems } // Ascending order of solved problems
         .thenByDescending { it.penalties } // Descending order of penalties
         .thenByDescending { it.login } // Descending order of login
-    val participantHeap = Heap(participantsComparator)
 
     val participantsCount = reader.readLine().toInt()
+    val participantsHeap = mutableListOf<Participant>()
+
     repeat(participantsCount) {
         val (login, solvedProblems, penalties) = reader.readLine().split(" ")
-        participantHeap.add(Participant(login, solvedProblems.toInt(), penalties.toInt()))
+        participantsHeap.add(Participant(login, solvedProblems.toInt(), penalties.toInt()), participantsComparator)
     }
 
     repeat(participantsCount) {
-        writer.writeLine(participantHeap.popMax().login)
+        writer.writeLine(participantsHeap.popMax(participantsComparator).login)
     }
     writer.flush()
 }
